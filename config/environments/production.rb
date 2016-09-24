4e7f544dc6c4e274b7ba4eb875710bd66c220097
macsandro <sandroucb@gmail.com>
2016-09-24 09:05:43 -0400

    Change Heroku address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.eager_load = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'https://sandro-saas-app.herokuapp.com', :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'https://sandro-saassoftware.herokuapp.com/', :protocol => 'https'}
 
 
   # Full error reports are disabled and caching is turned on.
